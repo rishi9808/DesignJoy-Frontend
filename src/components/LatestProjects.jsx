cba5f5c1cb474e8e984b8abdf864d53f7327923b
rishi9808 <rishikeshkaroth@gmail.com>
2025-06-30 16:31:32 +0530

    feat: add main project image to LatestProjects component and update image styles

--- src/components/LatestProjects.jsx
+++ src/components/LatestProjects.jsx
@@ -8,12 +8,12 @@ import LP3 from "../assets/LatestProjects/lp-3.jpg"
 import LP4 from "../assets/LatestProjects/lp-4.jpg"
 import LP5 from "../assets/LatestProjects/project-img.avif"
 import LP6 from "../assets/LatestProjects/lp-6.png"
-
+import banner from "../assets/LatestProjects/main.png"
 
 // import badge from "../assets/LatestProjects/badge.svg"
 
 function LatestProjects() {
-  const projects = [LP1, LP2, LP3, LP4, LP5, LP6];
+  const projects = [LP1, LP2, LP3, LP4, LP5, LP6, banner]
 
   return (
     <div className={styles.latestProjects}>
@@ -27,9 +27,8 @@ function LatestProjects() {
         {projects.map((project, index) => (
           <img
             key={index}
-            // src={project}
-            src={LP6}
-            alt={`Latest project ${index + 1}`}
+            src={banner}
+            alt="Project banner"
             className={styles.lpImg}
           />
         ))}
