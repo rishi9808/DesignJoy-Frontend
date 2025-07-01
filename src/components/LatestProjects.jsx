c325f97cebd39e5509397d7f8fa7de6ab273bc27
rishi9808 <rishikeshkaroth@gmail.com>
2025-07-01 20:03:22 +0530

    fix: remove unused project images from LatestProjects component

--- src/components/LatestProjects.jsx
+++ src/components/LatestProjects.jsx
@@ -1,19 +1,12 @@
-import React from 'react'
 import Marquee from "react-fast-marquee"
 import styles from "../styles/latestProjects.module.css"
 
-import LP1 from "../assets/LatestProjects/lp-1.jpg"
-import LP2 from "../assets/LatestProjects/lp-2.jpg"
-import LP3 from "../assets/LatestProjects/lp-3.jpg"
-import LP4 from "../assets/LatestProjects/lp-4.jpg"
-import LP5 from "../assets/LatestProjects/project-img.avif"
-import LP6 from "../assets/LatestProjects/lp-6.png"
 import banner from "../assets/LatestProjects/main.png"
 
 // import badge from "../assets/LatestProjects/badge.svg"
 
 function LatestProjects() {
-  const projects = [LP1, LP2, LP3, LP4, LP5, LP6, banner]
+  const projects = [banner, banner]
 
   return (
     <div className={styles.latestProjects}>
